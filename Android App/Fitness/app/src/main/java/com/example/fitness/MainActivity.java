d5fa7c961f3eb30a65bbbaa6d741ebe84f7faef8
Jamie Nolan <Jamie.Nolan@ibm.com>
2020-03-24 23:00:39 +0000

    Import HTTP Parser - okhttp
    Very popular in it's simple use for HTTP get, post and put requests for android

--- Android App/Fitness/app/src/main/java/com/example/fitness/MainActivity.java
+++ Android App/Fitness/app/src/main/java/com/example/fitness/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.fitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
