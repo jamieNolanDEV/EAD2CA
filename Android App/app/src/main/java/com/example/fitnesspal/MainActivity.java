e23114a0263af0363e935e78454a5f441beb191b
Jamie Nolan <Jamie.Nolan@ibm.com>
2020-04-19 23:08:04 +0100

    Bugfix - Android Manifest NULL error reallocation

--- Android App/app/src/main/java/com/example/fitnesspal/MainActivity.java
+++ Android App/app/src/main/java/com/example/fitnesspal/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         BFI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ViewBMI.class);
+                Intent intent = new Intent(MainActivity.this, ViewBMR.class);
                 startActivity(intent);
             }
         });
